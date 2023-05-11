94e40b1e00584f70c48915b0ba9074c4064ddbce
MosabShaheen <99540632+MosabShaheen@users.noreply.github.com>
2023-05-11 12:40:13 +0500

    initial commit

--- src/sections/banner/index.tsx
+++ src/sections/banner/index.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
     <section className="grid gap-4 text-center py-10 md:py-16 border-b bg-gradient-to-r from-rose-100 to-teal-100 dark:from-rose-100/80 dark:to-teal-100/80 text-stone-900">
       <h1 className="text-4xl md:text-6xl font-black px-6">Giga Stack ✨</h1>
       <p className="md:text-lg font-semibold text-stone-900 px-6">
-        The most Twitter influenced stack you've ever seen
+        {`The most Twitter influenced stack you've ever seen`}
       </p>
       <div className="flex gap-5 justify-center">
         <Link
