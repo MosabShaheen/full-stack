8c7de4db8c2b34f5428d7f2e4b7c7bd09a1ed0a0
MosabShaheen <99540632+MosabShaheen@users.noreply.github.com>
2023-05-16 16:14:34 +0500

    authentication solved

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs/dist/client-boundary/uiComponents";
 
-export default function Page() {
+export default function Page2() {
   return (
     <div className="container min-h-screen grid place-content-center">
-      <SignIn afterSignInUrl={"/dashboard"} signUpUrl="sign-up" />
+      <SignIn afterSignInUrl={"/dashboard"} signUpUrl="/sign-up" />
     </div>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,9 +1,9 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs/dist/client-boundary/uiComponents";
 
-export default function Page() {
+export default function Page1() {
   return (
     <div className="container min-h-screen grid place-content-center">
-      <SignUp afterSignUpUrl={"/dashboard"} signInUrl="sign-up" />
+      <SignUp afterSignUpUrl={"/dashboard"} signInUrl="/sign-in" />
     </div>
   );
 }
