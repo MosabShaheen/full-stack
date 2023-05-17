f47b3226f23a28129bfad45a812856cdffcaad5c
MosabShaheen <99540632+MosabShaheen@users.noreply.github.com>
2023-05-17 17:59:49 +0500

    database connected && tested

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs/dist/client-boundary/uiComponents";
 
 export default function Page2() {
   return (
-    <div className="container min-h-screen grid place-content-center">
+    <div className="container mx-auto min-h-screen grid place-content-center">
       <SignIn afterSignInUrl={"/dashboard"} signUpUrl="/sign-up" />
     </div>
   );
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs/dist/client-boundary/uiComponents";
 
 export default function Page1() {
   return (
-    <div className="container min-h-screen grid place-content-center">
+    <div className="container mx-auto min-h-screen grid place-content-center">
       <SignUp afterSignUpUrl={"/dashboard"} signInUrl="/sign-in" />
     </div>
   );
